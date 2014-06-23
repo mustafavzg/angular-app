f209754023d22ec572e6052d55a2697dc4d50c5f
Hari <harihari1989@gmail.com>
2014-06-23 18:03:24 +0530

    added the basic scrum update in users page: To do: - add backend

--- client/src/app/projects/users/users-itemview.js
+++ client/src/app/projects/users/users-itemview.js
@@ -44,10 +44,21 @@ angular.module('users-itemview',[
 		 **************************************************/
 		$scope.fetchingtasks = true;
 		$scope.tasks = [];
-		Tasks.forUser(
+		/*Tasks.forUser(
 			user.$id(),
 			function (tasks) {
 				$scope.tasks = tasks;
+				for(var i=0;i<tasks.length;i++){
+					$scope.tasks[i].propertiesToDisplay = [
+						{
+							name : 'Estimation',
+							value : $scope.tasks[i].estimation,
+							glyphiconclass : 'glyphicon glyphicon-time',
+							icon : 'time',
+							ordering : 1
+						}
+					];
+				}
 				$scope.fetchingtasks = false;
 			},
 			function (response) {
@@ -55,10 +66,101 @@ angular.module('users-itemview',[
 				// failed to fetch tasks
 				$scope.fetchingtasks = false;
 			}
-		);
+		);*/
+		$scope.tasks = [
+			{
+				Name : 'Sample Task 1',
+				Description : 'This is a sample task 1',
+				Priority : 1,
+				Estimation : 100,
+				propertiesToDisplay : [
+					{
+						name : 'Estimation',
+						value : 50,
+						glyphiconclass : 'glyphicon glyphicon-time',
+						icon : 'time',
+						ordering : 1
+					},
+					{
+						name : 'Priority',
+						value : 1,
+						glyphiconclass : 'glyphicon glyphicon-star',
+						icon : 'star',
+						ordering : 1
+					}
+				],
+				showAddButton : true
+			},
+			{
+				Name : 'Sample Task 2',
+				Description : 'This is a sample task 2',
+				Priority : 2,
+				Estimation : 100,
+				propertiesToDisplay : [
+					{
+						name : 'Estimation',
+						value : 100,
+						glyphiconclass : 'glyphicon glyphicon-time',
+						icon : 'time',
+						ordering : 1
+					},
+					{
+						name : 'Priority',
+						value : 1,
+						glyphiconclass : 'glyphicon glyphicon-star',
+						icon : 'star',
+						ordering : 1
+					}
+				],
+				showAddButton : true
+			}
+		];
+		$scope.scrumupdates = [
+			{
+				date : '06/17/2014',
+				text : 'Update for the day!!!'
+			},
+			{
+				date : '06/18/2014',
+				text : 'Update for the day!!!'
+			},
+			{
+				date : '06/19/2014',
+				text : 'Update for the day!!!'
+			},
+			{
+				date : '06/20/2014',
+				text : 'Update for the day!!!'
+			},
+			{
+				date : '06/21/2014',
+				text : 'Update for the day!!!'
+			}
+		];
 		$scope.taskscrudhelpers = {};
 		angular.extend($scope.taskscrudhelpers, crudListMethods('/projects/'+project.$id()+'/tasks'));
-
+		$scope.showAddButton = true;
+		$scope.saveScrumUpdate = function(task){
+			console.log("Scrum Update saved!!!");
+			task.showAddButton = true;
+		}
+		$scope.addScrumUpdate = function(task){
+			console.log("Adding Scrum Update!!!");
+			task.showAddButton = false;
+		}
+		$scope.startdate = "06/19/2014";
+		$scope.enddate = "06/21/2014";
+		$scope.dateRangeFilter = function(scrumUpdate){
+			console.log("Hi here (inside dateRangeFilter)!!!");
+			var scrumDate = new Date(scrumUpdate.date);
+			if($scope.startdate && $scope.enddate){
+				console.log("Hi here!!!");
+				var startDate = new Date($scope.startdate);
+				var endDate = new Date($scope.enddate);
+				return ((startDate <= scrumDate) && (endDate >= scrumDate));
+			}
+			return 1;
+		}
 		// $q.when(Tasks.forUser(user.$id())).then(
 		// 	function (tasks) {
 		// 		$scope.tasks = tasks;
