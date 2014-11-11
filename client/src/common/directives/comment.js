c6b93ae52488a1f239ba89738af81a5f48a56694
anunrs <anunrs@gmail.com>
2014-11-11 17:46:11 +0530

    Comments Directive : Tagging

--- client/src/common/directives/comment.js
+++ client/src/common/directives/comment.js
@@ -23,8 +23,7 @@ angular.module('directives.comment', [
 				forResource : '@',
 				resourceId : '@',
 				date: '='
-				//userId: '@'
-			},
+				},
 			controller: [
 				'$scope',
 				'$element',
@@ -34,6 +33,7 @@ angular.module('directives.comment', [
 				'$q',
 
 				function ($scope, $element, $attrs,Comments,security,$q) {
+					$scope.fetchingComments = true;
 					if(!$scope.comments)
 					{
 						$scope.comments = [];
@@ -68,15 +68,82 @@ angular.module('directives.comment', [
 							 	period ="PM";
 							}
 
-							var seconds= now.getSeconds();
-							var time= hour+"."+minutes+"."+seconds+"."+now.getMilliseconds()+" "+period;
+							var time= hour+"."+minutes+" "+period;
 							var months = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
 							var month= months[now.getMonth()];
 							tagObj = {};
 							var tagKey = Comments.getResourceKey(forResource);
 							tagObj[tagKey] = resourceId;
 							comment = {};
-							comment.timeStamp =date+"-"+month+"-"+year+" "+time;
+							comment.timeStampDate =date+"-"+month+"-"+year+" "+time;
+							var firstPartOfUrl = window.location.pathname.match(/\/projects\/.*\//);
+							if (commentVal.indexOf("#") != -1 ) 
+									{
+										for (var i=0; i<commentVal.length;i++)
+										{
+											if(commentVal[i] === "#" && commentVal[i-1] != ">")
+											{
+												
+												switch(commentVal.charAt(i+1))
+												{
+													case 'T' : {
+														var regex = /(^|\s)#T([^\s]+)/;
+														var match = regex.exec(commentVal);
+														comment.taskId = match[2];
+														console.log("\n taskId..");
+														console.log(comment.taskId);
+														commentVal=commentVal.replace(/(^|\s)#T([^\s]+)/g,' <a href=\"'+firstPartOfUrl+'tasks\/'+"$2"+'\">#T$2</a>');
+														break;
+													}
+													case 'B' : {
+														var regex = /(^|\s)#B([^\s]+)/;
+														var match = regex.exec(commentVal);
+														comment.backlogItemId = match[2];
+														console.log("\n backlogItemId..");
+														console.log(comment.backlogItemId);
+														commentVal=commentVal.replace(/(^|\s)#B([^\s]+)/g,' <a href=\"'+firstPartOfUrl+'productbacklog\/'+"$2"+'\">#B$2</a>');
+														break;
+													}
+													case 'S' : {
+														var regex = /(^|\s)#S([^\s]+)/;
+														var match = regex.exec(commentVal);
+														comment.sprintId = match[2];
+														console.log("\n sprintId..");
+														console.log(comment.sprintId);
+														commentVal=commentVal.replace(/(^|\s)#S([^\s]+)/g,' <a href=\"'+firstPartOfUrl+'sprints\/'+"$2"+'\">#S$2</a>');
+														break;
+													}
+													case 'P' : {
+														var regex = /(^|\s)#P([^\s]+)/;
+														var match = regex.exec(commentVal);
+														comment.projectId = match[2];
+														console.log("\n projectId..");
+														console.log(comment.projectId);
+														commentVal=commentVal.replace(/(^|\s)#P([^\s]+)/g,' <a href=\"\/projects\/'+"$2"+'\">#P$2</a>');
+														break;
+													}
+
+												}
+											}
+										}
+																	
+									}
+								
+							if (commentVal.indexOf("@") != -1 ) 
+									{
+										for (var i=0; i<commentVal.length;i++)
+										{
+											if(commentVal[i] === "@" && commentVal[i-1] != ">")
+											{
+												var regex = /(^|\s)@([^\s]+)/;
+												var match = regex.exec(commentVal);
+												comment.user = match[2];
+												console.log("\n User..");
+												console.log(comment.user);
+												commentVal=commentVal.replace(/(^|\s)@([^\s]+)/g,' <a href=\"'+firstPartOfUrl+'users\/'+"$2"+'\">@$2</a>');
+											}
+										}
+									}
 							comment.text = commentVal;
 							comment.createdBy = ($scope.currentUser)? $scope.currentUser.Username : "";
 							comment.hiveUserProfileId = ($scope.currentUser)? $scope.currentUser.ID : "";;
@@ -85,7 +152,7 @@ angular.module('directives.comment', [
 							console.log(comment);
 
 							var commentObj = new Comments(comment);
-							$scope.comments.push({text : comment.text,  createdBy: comment.createdBy, timeStamp: comment.timeStamp});
+							$scope.comments.push({text : comment.text,  createdBy: comment.createdBy, timeStampDate: comment.timeStampDate});
 							var successcb = function(){
 								console.log("saved successfully!!!");
 							};
@@ -106,11 +173,13 @@ angular.module('directives.comment', [
 			 			forResource,
 			 			resourceId,
 			 			function (comments) {
-							$scope.comments = comments;
-							console.log("comments fetchec");
+			 				$scope.comments = comments;
+			 				$scope.fetchingComments = false;
+							console.log("comments fetched");
 							console.log($scope.comments);
 						},
 						function (response) {
+
 						}
 					);
 					$scope.clearComment = function(resource){
